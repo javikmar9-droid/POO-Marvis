f1f56e2237966bff540f06fcb2f0f60765e22dc2
Javikmar9-doid <javikmar9@gmail.com>
2026-08-16 14:54:01 -0300

    Primer programa HolaMundo

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+
+/**
+ * HolaMundo
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola clase de POO");
+        System.out.println("IV Semestre 2026");
+    }
+}
