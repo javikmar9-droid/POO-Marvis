426d91041ff40bd0cb3326444189ac0e74b6df19
javikmar9-droid <javikmar9@gmail.com>
2026-08-27 22:03:10 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear un objeto de la clase Mueble
+        Muebleria miMueble = new Muebleria("Mesa Comedor", "Madera de Roble", 185000.0);
+
+        // Mostrar la información por consola
+        miMueble.mostrarInfo();
+    }
+}
